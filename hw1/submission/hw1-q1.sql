324be9f2e8ac49fe6332a09ba84359c2c301aa66
bogyshi <avanroi1@binghamton.edu>
2019-01-14 23:33:17 -0500

    done

--- hw1/submission/hw1-q1.sql
+++ hw1/submission/hw1-q1.sql
@@ -18,4 +18,5 @@ insert into Edges
 /*
 "SQLite supports the concept of "type affinity" on columns. The type affinity of a column is the recommended type for data stored in that column. The important idea here is that the type is recommended, not required. Any column can still store any type of data. It is just that some columns, given the choice, will prefer to use one storage class over another. The preferred storage class for a column is called its "affinity". " ~ sqlite.org/datatype3.html
 As we can see, since sqlite is not a static and rigid container for these values in the columns like most other SQL data base engines, it allows for the last insert to operate.
+Answer found from here : http://www.sqlite.org/datatype3.html
 */
